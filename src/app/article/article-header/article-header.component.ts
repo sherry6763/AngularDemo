b64e1d922c5f8ea696e988d6df9d015a07cc4801
sherryma <sherry6763@yahoo.com.tw>
2022-05-30 13:36:09 +0800

    @Output (event binding)

--- src/app/article/article-header/article-header.component.ts
+++ src/app/article/article-header/article-header.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { outputAst } from '@angular/compiler';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-article-header',
@@ -10,9 +11,17 @@ export class ArticleHeaderComponent implements OnInit {
   @Input()
   item;
 
+  @Output()
+  delete = new EventEmitter<any>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  deleteArticle() {
+    //emit方法，發送的意思，參數為要傳送的事件資料(預設時宣告為任意型別)
+    this.delete.emit(this.item);
   }
 
 }
