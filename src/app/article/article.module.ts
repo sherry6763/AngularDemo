b237fe45a51877fd3c2b7785b713836bc4b9b0b5
sherryma <sherry6763@yahoo.com.tw>
2022-05-30 09:50:44 +0800

    slice two child(AtricleHeader/ArticleBody) form parent(ArticleList)

--- src/app/article/article.module.ts
+++ src/app/article/article.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArticleListComponent } from './article-list/article-list.component';
+import { ArticleHeaderComponent } from './article-header/article-header.component';
+import { ArticleBodyComponent } from './article-body/article-body.component';
 
 
 
 @NgModule({
   declarations: [
-    ArticleListComponent
+    ArticleListComponent,
+    ArticleHeaderComponent,
+    ArticleBodyComponent
   ],
   imports: [
     CommonModule
